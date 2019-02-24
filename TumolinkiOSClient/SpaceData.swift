1af1e432a5a2007e7f8b81a9310c772b5f816699
Terashima <yamanogomora@gmail.com>
2019-02-25 08:55:14 +0900

    Add: スペースを管理するデータモデルを実装

---
+++ TumolinkiOSClient/SpaceData.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceData.swift
+//  TumolinkiOSClient
+//
+//  Created by 寺島 洋平 on 2019/02/25.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import Foundation
+
+class SpaceData {
+    // スペース画像のURL
+    var spaceImageUrl: String?
+    // スペース名
+    var spaceName: String?
+    // 今日のツモリスト
+}
